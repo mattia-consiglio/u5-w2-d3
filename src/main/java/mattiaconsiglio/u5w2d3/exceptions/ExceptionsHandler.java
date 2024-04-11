aa609757baf3bd5afe102cd2e9deb39a6dfa5ad3
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-04-11 17:35:56 +0200

    feat(exceptions): add handlers for IOException and NoResourceFoundException
    - Add ExceptionHandler for IOException with HTTP 400 Bad Request status
    - Add ExceptionHandler for NoResourceFoundException with HTTP 404 Not Found status
    - Include resource path in the error message for NoResourceFoundException

--- src/main/java/mattiaconsiglio/u5w2d3/exceptions/ExceptionsHandler.java
+++ src/main/java/mattiaconsiglio/u5w2d3/exceptions/ExceptionsHandler.java
@@ -7,7 +7,9 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
@@ -35,6 +37,18 @@ public class ExceptionsHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDTO handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         return new ErrorDTO("The server did not understand the request. Check the correct format of the JSON passed, their keys and values.", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleIOException(IOException e) {
+        return new ErrorDTO("There is error with the file provided", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDTO handleNoResourceFoundException(NoResourceFoundException e) {
+        return new ErrorDTO("The endpoint " + e.getResourcePath() + " not found", LocalDateTime.now());
     }
 
     @ExceptionHandler(RuntimeException.class)
